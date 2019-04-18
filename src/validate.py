034d2aa61f224f700837c87a20ed74e518a7b826
yuan <yrw@sdec.com.cn>
2019-04-18 16:30:16 +0800

    Modifying.

--- src/validate.py
+++ src/validate.py
@@ -5,6 +5,9 @@ from recommender import UserBased
 ubcf = UserBased()
 ubcf.loadData(trainSet)
 model = ubcf.loadExtModel('ubcf.model')
-import validation
-result = validation.evaluateRecommender(testSet, ubcf, model=model, topN=10)
+from validation import CrossValidation
+cv=CrossValidation()
+import similarity
+simMeasure = similarity.cosine_intersection
+result = cv.LeaveOneOut(trainSet,ubcf,model= model,simMeasure=simMeasure)
 print(result)
